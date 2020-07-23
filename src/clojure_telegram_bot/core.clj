95de76000f0e27ba1e5af55ab8de6f3c2e6c7e43
Mikhail <mm.leontev@gmail.com>
2020-07-23 15:44:55 +0300

    wip

--- src/clojure_telegram_bot/core.clj
+++ src/clojure_telegram_bot/core.clj
@@ -19,11 +19,20 @@
         text (get-in update [:message :text])]
   (defn fetch-url [url]
     (html/html-resource (java.net.URL. url)))
-  (def goog-news (fetch-url "https://example.com"))
-  (def html (nth goog-news 1))
-  (def result (html/text (nth (html/select html [:html :body :div :h1]) 0)))
-  (cg/send-message bot chat-id result))
-  )
+  (def html (fetch-url "https://hh.ru/search/vacancy?schedule=remote&clusters=true&area=1&no_magic=true&enable_snippets=true&salary=&st=searchVacancy&fromSearch=true&text=Frontend+%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA&from=suggest_post"))
+
+  (def body (get-in (nth (html/select html [:html :body]) 0) [:content]))
+  (def div-main (nth (get-in (nth (html/select body [:div.HH-Supernova-MainContent]) 0) [:content]) 0))
+  (def div-bloko-columns-wrapper (nth (html/select
+      (html/select div-main [:div :div :div :div]) [:div.bloko-columns-wrapper]) 2))
+  (def div-sticky-container (get-in (nth (html/select (html/select (get-in div-bloko-columns-wrapper [:content]) [:div :div])
+      [:div.sticky-container]) 0) [:content] ))
+          
+  (println "html" div-sticky-container)
+
+  ;; (cg/send-message bot chat-id result))
+  ))
+
 
 (defn -main
   "main function"
