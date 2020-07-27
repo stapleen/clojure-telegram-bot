c654fb0b59885d3784ac99d29661b5faf7ee4e58
Mikhail <mm.leontev@gmail.com>
2020-07-27 16:16:46 +0300

    wip

--- src/clojure_telegram_bot/core.clj
+++ src/clojure_telegram_bot/core.clj
@@ -13,25 +13,21 @@
 
 (defn echo
   ""
-  [bot update]
-  (let [chat-id (get-in update [:message :chat :id])
-        reply-to (get-in update [:message :message-id])
-        text (get-in update [:message :text])]
+[bot update]
+(let [chat-id (get-in update [:message :chat :id])
+      reply-to (get-in update [:message :message-id])
+      text (get-in update [:message :text])]
   (defn fetch-url [url]
     (html/html-resource (java.net.URL. url)))
   (def html (fetch-url "https://hh.ru/search/vacancy?schedule=remote&clusters=true&area=1&no_magic=true&enable_snippets=true&salary=&st=searchVacancy&fromSearch=true&text=Frontend+%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA&from=suggest_post"))
 
-  (def body (get-in (nth (html/select html [:html :body]) 0) [:content]))
-  (def div-main (nth (get-in (nth (html/select body [:div.HH-Supernova-MainContent]) 0) [:content]) 0))
-  (def div-bloko-columns-wrapper (nth (html/select
-      (html/select div-main [:div :div :div :div]) [:div.bloko-columns-wrapper]) 2))
-  (def div-sticky-container (get-in (nth (html/select (html/select (get-in div-bloko-columns-wrapper [:content]) [:div :div])
-      [:div.sticky-container]) 0) [:content] ))
-          
-  (println "html" div-sticky-container)
-
+  (def div-main-content (html/select html [:html :body :div.HH-Supernova-MainContent :div.main-content-wrapper :div.main-content]))
+  (def div-column-container (html/select div-main-content [:div :div.bloko-columns-wrapper :div.bloko-column_container]))
+  (def hh-content (html/select div-column-container [:div.bloko-column_l-16 :div.sticky-container :div.HH-StickyParentAreaResizer-Content]))
+  (def vacancy-results (html/select hh-content [:div.HH-SearchVacancyDropClusters-XsHiddenOnClustersOpenItem :div.bloko-column_xs-4 :div.bloko-gap_l-top :div :div.vacancy-serp]))
+  (println "html" (html/select vacancy-results [:div.vacancy-serp-item]))
   ;; (cg/send-message bot chat-id result))
-  ))
+    ))
 
 
 (defn -main
