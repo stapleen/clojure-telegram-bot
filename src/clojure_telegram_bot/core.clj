6638a5d2010a26cc554ae9087bafabcf0a3928a8
Mikhail <mm.leontev@gmail.com>
2020-08-27 13:21:30 +0300

    update readme

--- src/clojure_telegram_bot/core.clj
+++ src/clojure_telegram_bot/core.clj
@@ -24,7 +24,7 @@
     (def html (fetch-url "https://hh.ru/search/vacancy?schedule=remote&clusters=true&area=1&no_magic=true&enable_snippets=true&salary=&st=searchVacancy&fromSearch=true&text=Frontend+%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA&from=suggest_post"))
     (def links (html/select html [:div.vacancy-serp-item__info :a]))
     (def vacancy-url (map #(get-in % [:attrs :href]) (html/select html [:div.vacancy-serp-item__info :a])))
-   
+
     (cg/send-message bot chat-id (nth vacancy-url 0))))
 
 (defn -main
