4d719bec5270b10ad1d89c83e2c4a7c6e9a9105d
Mikhail <mm.leontev@gmail.com>
2020-09-02 16:14:37 +0300

    insert url in db & wip

--- src/clojure_telegram_bot/core.clj
+++ src/clojure_telegram_bot/core.clj
@@ -5,7 +5,7 @@
             [clojure.string :as str]
             [meinside.clogram :as cg]
             [net.cgrand.enlive-html :as html]
-            [clojure.java.jdbc :as j]
+            [clojure.java.jdbc :as jdbc]
             [clojure-telegram-bot.config :as config]))
 
 (def token config/token)
@@ -16,27 +16,48 @@
 
 (def db config/db-config)
 
-(defn html-parsing
-  ""
+(defn insert-vacancy-url
+  "insert urls in db"
+  [vacancy-url]
+  (def url (nth vacancy-url 0))
+  (jdbc/insert! db :vacancies {:vacancy_url url}))
+
+(defn bot-response
+  "bot response"
   [bot update]
   (let [chat-id (get-in update [:message :chat :id])
         reply-to (get-in update [:message :message-id])
         text (get-in update [:message :text])]
 
-    (defn fetch-url [url]
-      (html/html-resource (java.net.URL. url)))
+  (println "message" text)
+    ;; (cg/send-message bot chat-id (nth vacancy-url 0))
+    ))
 
-    (def html (fetch-url "https://hh.ru/search/vacancy?schedule=remote&clusters=true&area=1&no_magic=true&enable_snippets=true&salary=&st=searchVacancy&fromSearch=true&text=Frontend+%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA&from=suggest_post"))
-    (def links (html/select html [:div.vacancy-serp-item__info :a]))
-    (def vacancy-url (map #(get-in % [:attrs :href]) (html/select html [:div.vacancy-serp-item__info :a])))
+;; (defn html-parsing
+;;   "function for parsing html"
+;;   [bot update]
+;;   (let [chat-id (get-in update [:message :chat :id])
+;;         reply-to (get-in update [:message :message-id])
+;;         text (get-in update [:message :text])]
 
-    (cg/send-message bot chat-id (nth vacancy-url 0))))
+;;     (defn fetch-url [url]
+;;       (html/html-resource (java.net.URL. url)))
+
+;;     (def html (fetch-url "https://hh.ru/search/vacancy?schedule=remote&clusters=true&area=1&no_magic=true&enable_snippets=true&salary=&st=searchVacancy&fromSearch=true&text=Frontend+%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA&from=suggest_post"))
+;;     (def links (html/select html [:div.vacancy-serp-item__info :a]))
+;;     (def vacancy-url (map #(get-in % [:attrs :href]) (html/select html [:div.vacancy-serp-item__info :a])))
+;;     (insert-vacancy-url vacancy-url)
+
+;;     ;; (cg/send-message bot chat-id (nth vacancy-url 0))
+;;     ))
+
+
 
 (defn -main
   "main function"
   [& _]
   (println ">>> launching application...")
 
-  (println (j/query db ["SELECT * FROM vacancies"]))
+  ;; (println (jdbc/query db ["SELECT * FROM vacancies"]))
 
-  (cg/poll-updates bot interval html-parsing))
+  (cg/poll-updates bot interval bot-response))
