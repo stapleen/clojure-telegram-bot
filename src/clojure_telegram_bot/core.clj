cef1207488570ff5db6dc1394e0209bbbc76be5c
Mikhail <mm.leontev@gmail.com>
2020-09-03 17:42:27 +0300

    add do-short-links

--- src/clojure_telegram_bot/core.clj
+++ src/clojure_telegram_bot/core.clj
@@ -19,29 +19,34 @@
 (def url-vacancy-min-length 0)
 (def url-vacancy-max-length 30)
 
-(defn insert-vacancy-url
-  "insert urls in db"
-  [vacancy-url]
+(defn do-short-links
+  "shorten links length"
+  [vacancies-url]
+  (mapv #(subs % url-vacancy-min-length url-vacancy-max-length) vacancies-url))
 
-  (def url-list (mapv #(subs % url-vacancy-min-length url-vacancy-max-length) vacancy-url))
-  (jdbc/insert-multi! db :vacancies [:vacancy_url] (mapv #(vector %) url-list)))
+(defn insert-vacancies-urls
+  "insert urls in db"
+  [vacancies-url chat-id]
+  (def url-list (do-short-links vacancies-url))
+
+  (jdbc/insert-multi! db :vacancies [:vacancy_url] (mapv #(vector %) url-list))
+  (cg/send-message bot chat-id "Список вакансий сформирован"))
 
 (defn html-parsing
   "function for parsing html"
-  []
+  [chat-id]
   (defn fetch-url [url] (html/html-resource (java.net.URL. url)))
 
   (def html (fetch-url url-hh))
   (def links (html/select html [:div.vacancy-serp-item__info :a]))
-  (def vacancy-url (mapv #(get-in % [:attrs :href]) links))
+  (def vacancies-url (mapv #(get-in % [:attrs :href]) links))
 
-  (insert-vacancy-url vacancy-url))
+  (insert-vacancies-urls vacancies-url chat-id))
 
 (defn mark-vacancy-viewed
   "mark vacancy as view in the database"
   [id]
-  (jdbc/update! db :vacancies {:is_show 1} ["id = ?" id])
-  )
+  (jdbc/update! db :vacancies {:is_show 1} ["id = ?" id]))
 
 (defn get-vacancies
   "send list of vacancies"
@@ -62,7 +67,7 @@
         text (get-in update [:message :text])]
 
     (cond
-      (= text "/new") (html-parsing)
+      (= text "/new") (html-parsing chat-id)
       (= text "/get") (get-vacancies chat-id)
       )))
 
