9555535ffff1cff11442dd2fb31b4b6157ca0814
Mikhail <mm.leontev@gmail.com>
2020-09-04 17:54:51 +0300

    wip

--- src/clojure_telegram_bot/core.clj
+++ src/clojure_telegram_bot/core.clj
@@ -19,7 +19,7 @@
 (def url-vacancy-min-length 0)
 (def url-vacancy-max-length 30)
 
-(def info "Список доступных команд\n/set - отправить ссылку на поиск вакансий\n/update - обновить список вакансий\n/get - получить новые вакансии\n/cancel - сбросить просмотренные вакансии")
+(def info "Список доступных команд:\n/set - отправить ссылку на поиск вакансий\n/update - обновить список вакансий\n/get - получить новые вакансии\n/cancel - сбросить просмотренные вакансии")
 
 (defn do-short-links
   "shorten links length"
