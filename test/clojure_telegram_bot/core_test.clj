e63d21335f86956b317adc5fc8da01ba17c1c955
Mikhail <mm.leontev@gmail.com>
2020-09-10 11:29:58 +0300

    update test

--- test/clojure_telegram_bot/core_test.clj
+++ test/clojure_telegram_bot/core_test.clj
@@ -5,8 +5,9 @@
 
 (deftest parse-html-test-first
   (testing "parse-html-test-first failed"
-    (is (= "https://hh.ru/vacancy/39014335?query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA" (-> "./test/clojure_telegram_bot/fixtures/index.html" java.io.File. html/html-resource parse-html last)))))
+    (is (= "https://hh.ru/vacancy/38949794?query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA" (-> "./test/clojure_telegram_bot/fixtures/index.html" java.io.File. html/html-resource parse-html first)))))
 
 (deftest parse-html-test-last
   (testing "parse-html-test-last failed"
-    (is (= "https://hh.ru/vacancy/38949794?query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA" (-> "./test/clojure_telegram_bot/fixtures/index.html" java.io.File. html/html-resource parse-html first)))))
+    (is (= "https://hh.ru/vacancy/39014335?query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA" (-> "./test/clojure_telegram_bot/fixtures/index.html" java.io.File. html/html-resource parse-html last)))))
+
