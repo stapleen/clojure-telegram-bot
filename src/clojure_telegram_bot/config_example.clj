d36ab3c10f3dfd15f78bcd4f3e1dc567006b29e0
Mikhail <mm.leontev@gmail.com>
2020-09-10 11:25:59 +0300

    update config

--- src/clojure_telegram_bot/config_example.clj
+++ src/clojure_telegram_bot/config_example.clj
@@ -1,10 +1,11 @@
 (ns clojure-telegram-bot.config)
 
-(def token "12312:AAAAAAAAAA")
+(def token "111111:AAAAAAAA")
 (def interval 1)
 (def verbose? true)
 
-(def db-config {:dbtype "db_type"
-                :dbname "db_name"
+(def db-config {:dbtype "dbtype"
+                :dbname "dbname"
+                :host "host"
                 :user "user"
                 :password "password"})
